a3e4cdbf5e088f24169f192709186b50b8158ca5
joseph704 <freelove54@naver.com>
2019-05-28 12:54:57 +0900

    Initial Commit

---
+++ UNIX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UNIX
+//
+//  Created by 차요셉 on 28/05/2019.
+//  Copyright © 2019 차요셉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UNIXTests/UNIXTests.swift
@@ -0,0 +1,34 @@
+//
+//  UNIXTests.swift
+//  UNIXTests
+//
+//  Created by 차요셉 on 28/05/2019.
+//  Copyright © 2019 차요셉. All rights reserved.
+//
+
+import XCTest
+@testable import UNIX
+
+class UNIXTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
